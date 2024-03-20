8d52611b7be3e2adcb3e291b52cbc384c31e0e6b
weijiasu <weijiasu0804@gmail.com>
2024-03-20 01:15:42 -0400

    simulator

--- Fig3/read_simulator.py
+++ Fig3/read_simulator.py
@@ -13,6 +13,8 @@ parser=argparse.ArgumentParser()
 
 parser.add_argument("-Ref","--RefSeq")
 parser.add_argument("-out","--Outname")
+parser.add_argument("-nSeq","--NumberofSeq")
+parser.add_argument("-len_LTR","--LTRlength")
 args=parser.parse_args()
 
 reference=args.RefSeq
@@ -20,9 +22,11 @@ out=args.Outname
 TEseq=list(SeqIO.parse(reference,"fasta"))
 sequence=str(TEseq[0].seq)
 ID=TEseq[0].id
+nSeq=args.NumberofSeq
+len_LTR=args.LTRlength
 
 
-def simulate(front,end,start,stop,copy,direction,LTR,len_LTR):
+def simulate(front,end,copy,direction,LTR,len_LTR):
 	if LTR==1:
 		front=0
 		end=len(sequence)-len_LTR
@@ -33,52 +37,49 @@ def simulate(front,end,start,stop,copy,direction,LTR,len_LTR):
 		front=0
 	if LTR==3:
 		end=len(sequence)
+	start=random.randint(front,end)
+	stop=random.randint(front,end)
 	template=Seq(sequence[front:end])
 	first=Seq(sequence[front:start])
 	second=Seq(sequence[stop:end])
 	if direction=="+":
-		return str(second)+ str(template)*copty +str(first)
+		parameter=["LTR:"+str(LTR),front,end,start,stop,copy,direction]
+		return parameter,str(second)+ str(template)*copty +str(first)
 	if direction=="-":
-		return str(first.reverse_complement())+str(template.reverse_complement())*copy+str(second.reverse_complement())
+		return parameter,str(first.reverse_complement())+str(template.reverse_complement())*copy+str(second.reverse_complement())
 	
 
 def generate_(seq,filename,nSeq):
 	length=len(sequence)
 	f=open(filename,"w")
 	for i in range(0,nSeq):
-	for ltr in [1,2,5,3,0]:
-		for i in range(0,nSeq):
-			front=random.randint(0,length)
-			end=random.randint(front,length)
-			start=random.randint(front,end)
-			stop=random.randint(front,end)
-			direction=random.choice(["+","-"])
-			copy=random.randint(0,11)
-			direction=random.choice(["+","-"])
-			parameter=[start,end,middle,copy,stop,direction]
-			parameter=[str(c) for c in parameter]
-			name=filename+str(i+1)+"_"+"_".join(parameter)
-			read=Simulate(start,end,middle,copy,stop,direction)
-			f.write(">"+name+"\n"+read+"\n")
+		for ltr in [1,2,5,3,0]:
+			for i in range(0,nSeq):
+				front=random.randint(0,length)
+				end=random.randint(front,length)
+				copy=random.randint(0,11)
+				direction=random.choice(["+","-"])
+				parameter,read=Simulate(front,end,copy,direction,LTR,len_LTR)
+				name=filename+"_"+str(i+1)+"_"+"_".join(parameter)
+				f.write(">"+name+"\n"+read+"\n")
+	f.close()
 
-#def FalsePostive(filename,nSeq):
-#	f=open(filename,"w")
-#	for i in range(0,nSeq):
-#		length=len(sequence)
-#		r1=random.randint(0,length)
-#		r2=random.randint(0,length)
-#		start=min(r1,r2)
-#		end=max(r1,r2)
-#		template=sequence[start:end]
-#		copy=random.randint(0,11)
-#		amplify=template*copy
-#		parameter=[start,end,copy]
-#		parameter=[str(c) for c in parameter]
-#		name=filename+str(i+1)+"_"+"_".join(parameter)
-#		f.write(">"+name+"\n"+amplify+"\n")
-#
-generateFile(out+"_full.fa",0,len(sequence),1)
-r1=random.randint(0,len(sequence))
-r2=random.randint(0,len(sequence))
-generateFile(out+"_frag.fa",min((r1,r2)),max((r1,r2)),1)
-FalsePostive(out+"_FP.fa",1)
+def FalsePostive(filename,nSeq):
+	f=open(filename,"a")
+	for i in range(0,nSeq):
+		length=len(sequence)
+		r1=random.randint(0,length)
+		r2=random.randint(0,length)
+		start=min(r1,r2)
+		end=max(r1,r2)
+		template=sequence[start:end]
+		copy=random.randint(0,11)
+		amplify=template*copy
+		parameter=[start,end,copy]
+		parameter=[str(c) for c in parameter]
+		name=filename+"_"+str(i+1)+"_"+"FP_"+"_".join(parameter)
+		f.write(">"+name+"\n"+amplify+"\n")
+	f.close()
+
+generate_(sequence,out,nSeq)
+FalsePostive(out,nSeq)

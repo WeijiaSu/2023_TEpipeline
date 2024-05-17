e920b54c56cd654bc4bd4996e4cd70867b68fa8b
weijiasu <weijiasu0804@gmail.com>
2024-05-16 21:08:30 -0400

    update

--- Fig4/simulate_insertion.py
+++ Fig4/simulate_insertion.py
@@ -1,7 +1,11 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 import random
 import sys
 import os
+
+random.seed(42)
 
 def load_fasta(file_path,chromosome=None):
     genomefile=list(SeqIO.parse(file_path, "fasta"))
@@ -21,7 +25,7 @@ def select_genome(genome):
     genome_seq = random.choice(genome)
     random_point=random.randint(0, len(genome_seq.seq)-100000)
     random_lengh=random.randint(100,100000)
-    print("selected: %s(%s): %s - %s"%(genome_seq.id,len(genome_seq.seq),random_point,random_point+random_lengh))
+    #print("selected: %s(%s): %s - %s"%(genome_seq.id,len(genome_seq.seq),random_point,random_point+random_lengh))
     random_seq=genome_seq.seq[random_point:random_point+random_lengh]
     return [genome_seq.id,random_point,random_point+random_lengh,random_seq]
 
@@ -41,17 +45,17 @@ def main():
     
     # Simulate 100 insertions for each transposon in different genome sequences
     for transposon in transposon_sequences:
-        for _ in range(10):  # 100 insertions per transposon
+        for _ in range(100):  # 100 insertions per transposon
             # Choose a random genome sequence for each insertion
             print("Simulating %s #%s "%(transposon.id,_+1))
             genome=select_genome(genome_sequences)
-            genome.seq=genome[-1]
+            genome_seq=genome[-1]
 			# Choose a random insertion point within the genome sequence
-            insertion_point = random.randint(0, len(genome.seq))
+            insertion_point = random.randint(0, len(genome_seq))
             # Simulate insertion
-            genome.seq = genome.seq[:point] + transposon.seq + genome.seq[point:]
-            genome.id="_".joint(genome[0:3],transposon.id,str(insertion_point))
-            seq_record = SeqRecord(Seq(genome.seq), id=genome.id, description="")
+            genome_seq = genome_seq[:insertion_point] + transposon.seq + genome_seq[insertion_point:]
+            genome_id="_".join([str(i) for i in genome[0:3]]+[transposon.id,str(insertion_point)])
+            seq_record = SeqRecord(genome_seq, id=genome_id, description="")
             modified_genome_sequences.append(seq_record)
 
     output_file = sys.argv[3]
